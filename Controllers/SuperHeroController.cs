969f5bcb505a758987df48218d64d52cc4f42c7a
Pedro Lugon <pedromotta20@gmail.com>
2024-07-19 00:59:56 -0300

    feat(superhero): add getall function

---
+++ Controllers/SuperHeroController.cs
@@ -0,0 +1,31 @@
+using dotnet_study.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_study.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var heroes = new List<SuperHero>
+            {
+                new SuperHero
+                {
+                    Id = 1,
+                    Name = "Spiderman",
+                    FirstName = "Peter",
+                    LastName = "Parker",
+                    Place = "New York"
+                }
+            };
+
+            return Ok(heroes);
+        }
+
+
+    }
+}
